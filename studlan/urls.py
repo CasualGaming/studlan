bdd4f7564e29894c084e197012c3b0c07a7b9266
Jonas Svarvaa <=>
2011-11-17 03:16:07 +0100

    Fixed create_team form. It now gives no errors, but nothing happens serverside, which leads me to believe I derped somewhere...

--- studlan/urls.py
+++ studlan/urls.py
@@ -13,6 +13,7 @@ urlpatterns = patterns('',
     url(r'^competitions/', include('studlan.competition.urls')),
     url(r'^misc/remove_alert.html', 'studlan.misc.views.remove_alert'),
     url(r'^teams/', 'studlan.competition.views.teams', name='teams'),
+    url(r'^teams/create_team.html', 'studlan.competition.views.create_team'),
     # Examples:
     # url(r'^$', 'studlan.views.home', name='home'),
     # url(r'^studlan/', include('studlan.foo.urls')),
