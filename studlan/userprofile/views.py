a2536e9f5f4a9ee437475c5ae4b4ef75421ec345
Håvard Slettvold <havardsv@stud.ntnu.no>
2012-03-19 11:17:23 +0100

    Fixed a missing import.

--- studlan/userprofile/views.py
+++ studlan/userprofile/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.shortcuts import render, get_object_or_404
 
 from studlan.userprofile.forms import UserProfileForm
