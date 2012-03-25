524fcd30097066e7ceddb452ab83a4b8978826b6
Håvard Slettvold <havardsv@stud.ntnu.no>
2012-03-25 18:37:57 +0200

    Initial code for sponsor app.

---
+++ studlan/sponsor/models.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from django.db import models
+
+
+class Sponsor(models.Model):
+    name = models.CharField("name", max_length=50)
+    website = models.CharField("website", max_length=200)
+    priority = models.IntegerField("priority", 
+        help_text="higher priority means closer to the top of the sponsor list.")
+    
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['priority']
