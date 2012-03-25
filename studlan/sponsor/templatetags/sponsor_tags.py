99f60e8033d3a001172ddb1cf110c8484f100cf5
Håvard Slettvold <havardsv@stud.ntnu.no>
2012-03-25 19:23:33 +0200

    Made a proper sponsor templatetag in the new app.

---
+++ studlan/sponsor/templatetags/sponsor_tags.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from django import template
+
+from studlan.sponsor.models import Sponsor
+
+register = template.Library()
+
+@register.inclusion_tag('sponsor/sponsor_list.html')
+def show_sponsor_list():
+    sponsors = Sponsor.objects.all()
+
+    return {'sponsors': sponsors}
