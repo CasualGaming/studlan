512723f90d72ea8972b0d43856ed1ab22ef41313
Håvard Slettvold <havardsv@stud.ntnu.no>
2012-03-26 17:29:18 +0200

    Made a template tag to produce the team tabs.

--- studlan/team/templatetags/team_tags.py
+++ studlan/team/templatetags/team_tags.py
@@ -7,3 +7,7 @@ register = template.Library()
 @register.inclusion_tag('team/team_list.html')
 def team_list(teams):
     return {'teams': teams}
+
+@register.inclusion_tag('team/team_tabs.html')
+def team_tabs(active):
+    return {'active': active}
