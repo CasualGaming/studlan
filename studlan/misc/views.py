d4d5616dd6d41a112277a3498f26e2d8f4351f12
Håvard Slettvold <havardsv@stud.ntnu.no>
2012-03-27 23:26:55 +0200

    Renamed has_payed to has_paid. Also converted all indentations in views from tabs to spaces.

--- studlan/misc/views.py
+++ studlan/misc/views.py
@@ -7,33 +7,35 @@ from django.http import Http404
 
 # This is accessed asynchronously to remove alerts via jquery
 def remove_alert(request):
-	try:
-		pass
-	except:
-		pass
-	return HttpResponse('')
+    try:
+        pass
+    except:
+        pass
+    return HttpResponse('')
 
 def arrivals(request):
-	if not request.user.is_staff:
-		raise Http404
-	users = User.objects.all()
-	sorted_users = []
-	for u in users:
-		sorted_users.append(u)
-	sorted_users.sort(key=lambda x: x.username.lower(), reverse=False)
-	return render_to_response('arrivals.html', {'users': sorted_users}, context_instance=RequestContext(request))
+    if not request.user.is_staff:
+        raise Http404
+    users = User.objects.all()
+    sorted_users = []
+    for u in users:
+        sorted_users.append(u)
+    sorted_users.sort(key=lambda x: x.username.lower(), reverse=False)
+    return render_to_response('misc/arrivals.html', {'users': sorted_users}, context_instance=RequestContext(request))
 
 def toggle_arrival(request, user_id):
-	if not request.user.is_staff:
-		raise Http404
-	user = get_object_or_404(User, pk=user_id)
-	if user.get_profile().has_payed:
-		user.get_profile().has_payed = False
-	else:
-		user.get_profile().has_payed = True
-	user.get_profile().save()
-	user.save()
-	return redirect('arrivals')
+    if not request.user.is_staff:
+        raise Http404
+    user = get_object_or_404(User, pk=user_id)
+    if user.get_profile().has_paid:
+        print 'had paid'
+        user.get_profile().has_paid = False
+    else:
+        print 'had not paid'
+        user.get_profile().has_paid = True
+    user.get_profile().save()
+    user.save()
+    return redirect('arrivals')
 
 def handler404(request):
-	return render_to_response('404.html', context_instance=RequestContext(request))
+    return render_to_response('404.html', context_instance=RequestContext(request))
