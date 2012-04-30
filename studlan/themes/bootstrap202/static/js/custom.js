d437b6aca14963f482491eca33bae904c0022425
Jonas Svarvaa <svarvaa@online.ntnu.no>
2012-04-30 19:18:46 +0200

    Scrapped my expandable news item idea.

--- studlan/themes/bootstrap202/static/js/custom.js
+++ studlan/themes/bootstrap202/static/js/custom.js
@@ -1,19 +1,3 @@
-function toggleArticle(article) {
-    var curId = $(article).attr("id");
-    // Expand
-    if($("#"+curId).attr('style').indexOf("display: none;") >= 0) {
-        $("#"+curId).slideToggle("slow", "linear", function() {
-            $("#"+curId+"-icon").removeClass("icon-chevron-right").addClass("icon-chevron-down");
-        });
-    }
-    // Shrink
-    else {
-        $("#"+curId).slideToggle("slow", "linear", function() {
-            $("#"+curId+"-icon").removeClass("icon-chevron-down").addClass("icon-chevron-right");
-        });
-    }
-}
-
 $('.alert-message').alert();
 
 $.ajaxSetup ({  
