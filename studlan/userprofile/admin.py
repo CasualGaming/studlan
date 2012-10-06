403c0b8fd8936cc7c29b3fc72bd64230a89d3ada
melwil <slettvold@gmail.com>
2012-10-06 21:50:32 +0200

    Added UserProfile to user admin.

---
+++ studlan/userprofile/admin.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+from django.forms import models
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
+from studlan.userprofile.models import UserProfile
+
+admin.site.unregister(User)
+
+class NoDeleteInline(models.BaseInlineFormSet):
+    """ Custom formset to prevent deletion
+    Used by the inline for userprofiles to prevent the possibility
+    of deleting the profile object
+    """
+    def __init__(self, *args, **kwargs):
+        super(NoDeleteInline, self).__init__(*args, **kwargs)
+        self.can_delete = False
+
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    formset = NoDeleteInline
+
+class UserProfileAdmin(UserAdmin):
+    inlines = (UserProfileInline,)
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff',)
+    list_filter = ('groups', 'is_staff', 'is_superuser')
+    filter_horizontal = ('groups',)
+
+admin.site.register(User, UserProfileAdmin)
