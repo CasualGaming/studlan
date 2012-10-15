548fe62e2a99a99dd5875daa67eb7a7a1605a6bd
Jonas Svarvaa <jonassvarvaa@gmail.com>
2012-10-15 14:06:33 +0200

    Added single view for news items, as well as the needed urls-conf.

--- studlan/news/urls.py
+++ studlan/news/urls.py
@@ -5,4 +5,5 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('studlan.news.views',
     url(r'^$', 'main', name='news_main', kwargs={'page': 1}),
     url(r'^(?P<page>\d+)/$', 'main', name='news'),
+    url(r'^item/(?P<article_id>\d+)/$', 'single', name='news_single'),
 )
