9b460ee7dd8c3d66e93dc56d67101b8d0e11b667
melwil <slettvold@gmail.com>
2012-10-26 20:33:45 +0200

    Added a check for already existing team tag.

--- studlan/team/forms.py
+++ studlan/team/forms.py
@@ -4,6 +4,8 @@ import re
 
 from django import forms
 
+from studlan.team.models import Team
+
 class TeamCreationForm(forms.Form):
     title = forms.CharField(label="Title", max_length=50)
     tag = forms.CharField(label="Tag", max_length=10)
@@ -18,5 +20,9 @@ class TeamCreationForm(forms.Form):
             tag = cleaned_data['tag']
             if not re.match("^[a-zA-Z0-9_-]+$", tag):
                 self._errors['tag'] = self.error_class(["Your desired tag contains illegal characters. Valid: a-Z 0-9 - _"])
+            
+            team = Team.objects.filter(tag=tag)
+            if team.count() > 0:
+                self._errors['tag'] = self.error_class(["This team tag is taken. The team belongs to '%s'." % team[0].leader])
 
             return cleaned_data
