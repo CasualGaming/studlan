783be33b79b67760f29750368163e7b44a88d7ff
Håkon <torrib90@gmail.com>
2014-02-26 21:24:29 +0100

    Added admin links for the lottery

--- studlan/lottery/urls.py
+++ studlan/lottery/urls.py
@@ -8,6 +8,7 @@ urlpatterns = patterns('studlan.lottery.views',
     url(r'^$', 'index', name='index'),
     url(r'^signup/(?P<lottery_id>\d+)/$', 'sign_up', name='sign_up'),
     url(r'^signoff/(?P<lottery_id>\d+)/$', 'sign_off', name='sign_off'),
+    url(r'^drawing/$', 'drawing', name='drawing'),
     url(r'^drawing/(?P<lottery_id>\d+)/$', 'drawing', name='drawing'),
     url(r'^draw/(?P<lottery_id>\d+)/$', 'draw', name='draw'),
 )
