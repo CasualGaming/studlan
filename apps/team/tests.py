2fb58157dd89d84b8a882d3c916be4d84d14e0be
Håvard Slettvold <slettvold@gmail.com>
2014-10-29 01:15:49 +0000

    Created a simple test to see if it fixes cobertura in jenkins.

--- apps/team/tests.py
+++ apps/team/tests.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*-
+
 from django.test import TestCase
 
-# Create your tests here.
+class TeamTests(TestCase):
+
+    def test_something(self):
+        self.assertEqual(1 + 1, 2)
