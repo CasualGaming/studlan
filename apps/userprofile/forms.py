8f263cb5a63d843f70021bb4af60dc4993bb394b
larserikgk <larserikgk@gmail.com>
2014-12-21 02:38:31 -0500

    Removed 'wants to sit with' from presentation layer

--- apps/userprofile/forms.py
+++ apps/userprofile/forms.py
@@ -7,4 +7,4 @@ from apps.userprofile.models import UserProfile
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
-        fields = ('nick', 'ntnu_username', 'wants_to_sit_with', 'gender', 'date_of_birth', 'address', 'zip_code', 'phone',)
+        fields = ('nick', 'ntnu_username', 'gender', 'date_of_birth', 'address', 'zip_code', 'phone',)
