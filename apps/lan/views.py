c7f524e97e5a1c75e79b990d0a4a4e043237f999
Tor Håkon Bonsaksen <torrib90@gmail.com>
2014-12-30 22:53:09 +0100

    Added translatable for lan and tickettypes

--- apps/lan/views.py
+++ apps/lan/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.decorators import login_required, user_passes_test
 from django.http import Http404, HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 
-from apps.lan.models import LAN, Attendee
+from apps.lan.models import LAN, Attendee, Ticket
 
 def home(request):
     lans = LAN.objects.filter(end_date__gte=datetime.now())
@@ -20,23 +20,26 @@ def home(request):
 def listing(request):
     upcoming_lans = LAN.objects.filter(end_date__gte=datetime.now())
     previous_lans = LAN.objects.filter(end_date__lt=datetime.now())
-
+    
     return render(request, 'lan/list.html', {'upcoming': upcoming_lans, 'previous': previous_lans})
 
 def details(request, lan_id):
     lan = get_object_or_404(LAN, pk=lan_id)
 
+    ticket_types = lan.tickettype_set.all() 
+
+    user_tickets = Ticket.objects.filter(user=request.user, ticket_type__in=ticket_types)
+
     if request.user in lan.attendees:
-        if request.user in lan.paid_attendees:
+        if request.user in lan.paid_attendees or user_tickets:
             status = 'paid'
         else:
             status = 'attending'
     else:
         status = 'open'
 
-    ticket_types = lan.tickettype_set.all()
-
-    return render(request, 'lan/details.html', {'lan': lan, 'status': status, 'ticket_types': ticket_types})
+    return render(request, 'lan/details.html', {'lan': lan, 'status': status, 
+        'ticket_types': ticket_types, 'ticket': user_tickets})
 
 @login_required
 def attend(request, lan_id):
