f55e54538d8822fd1480d0906cdfa896ed52bd08
Lars Erik G-K <larserikgk@gmail.com>
2017-02-09 15:40:51 +0100

    Fix migration error introduced in 1.10 merge

--- apps/seating/migrations/0010_auto_20161202_1519.py
+++ apps/seating/migrations/0010_auto_20161202_1519.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('seating', '0009_merge'),
+        ('seating', '0009_auto_20161209_1448'),
     ]
 
     operations = [
