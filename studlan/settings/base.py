054843d3c8571dd1102660bfb1fa68b850ac682b
Mathias Flått <mathiafl@stud.ntnu.no>
2017-02-09 16:49:16 +0100

    fixes issue with static path, that were caused by stupidity

--- studlan/settings/base.py
+++ studlan/settings/base.py
@@ -113,7 +113,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/files/'
+STATIC_ROOT = 'static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -125,12 +125,12 @@ STATIC_URL = '/static/'
 ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT_DIRECTORY, "static")
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_ROOT_DIRECTORY, "files")
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-)
+]
 
 # List of finder classes that know how to find static files in
 # various locations.
@@ -149,9 +149,6 @@ ROOT_URLCONF = 'studlan.urls'
 
 WSGI_APPLICATION = 'studlan.wsgi.application'
 
-STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT_DIRECTORY, 'files'),
-]
 
 LOCALE_PATHS = (
     os.path.join(PROJECT_ROOT_DIRECTORY, 'locale'),
