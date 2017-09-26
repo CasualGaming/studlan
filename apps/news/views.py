029820f356e867560c8598994da1a1b558e5e0fb
Mathias Flått <mathiafl@stud.ntnu.no>
2017-09-26 23:09:07 +0200

    fetches active lans in plural, just in case

--- apps/news/views.py
+++ apps/news/views.py
@@ -12,33 +12,33 @@ from django.core.exceptions import ObjectDoesNotExist
 
 
 def main(request, page):
-    try:
-        active_lans = LAN.objects.filter(end_date__gte=datetime.now())
+    active_lans = LAN.objects.filter(end_date__gte=datetime.now())
+    if len(active_lans) > 0:
         articles = Article.objects.filter(relevant_to__in=active_lans).order_by('-pinned', 'pub_date')
-    except ObjectDoesNotExist:
-        return redirect('archive')
     else:
-        paginator = Paginator(articles, 10)  # Articles per page
-        streams = Stream.objects.filter(active=True).order_by('-pk')[:1]
+        return redirect('archive')
 
-        try:
-            articles = paginator.page(page)
-        except PageNotAnInteger:
-            # If page is not an integer, deliver first page.
-            articles = paginator.page(1)
-        except EmptyPage:
-            # If page is out of range (e.g. 9999), deliver last page of results.
-            articles = paginator.page(paginator.num_pages)
-        except:
-            # If no page is given, show the first
-            articles = paginator.page(1)
+    paginator = Paginator(articles, 10)  # Articles per page
+    streams = Stream.objects.filter(active=True).order_by('-pk')[:1]
 
-        if len(streams) > 0:
-            return render(request, 'news/news.html', {'articles': articles, 'page': page, 'stream': streams[0],
-                                                      'languages': settings.LANGUAGES})
-        else:
-            return render(request, 'news/news.html', {'articles': articles, 'page': page, 'languages':
-                                                        settings.LANGUAGES})
+    try:
+        articles = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        articles = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        articles = paginator.page(paginator.num_pages)
+    except:
+        # If no page is given, show the first
+        articles = paginator.page(1)
+
+    if len(streams) > 0:
+        return render(request, 'news/news.html', {'articles': articles, 'page': page, 'stream': streams[0],
+                                                  'languages': settings.LANGUAGES})
+    else:
+        return render(request, 'news/news.html', {'articles': articles, 'page': page, 'languages':
+                                                    settings.LANGUAGES})
 
 
 def single(request, article_id):
