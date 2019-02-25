d816b80785ccaca8b2122c05943ac119f511280a
Håvard O. Nordstrand <haavard.nordstrand@gmail.com>
2019-02-26 00:42:16 +0100

    Don't add unique Docker tag

--- manage/deploy-travis.sh
+++ manage/deploy-travis.sh
@@ -2,24 +2,31 @@
 # Syntax: deploy-travis.sh [extra_tag]*
 
 if [[ $TRAVIS_BRANCH != "true" ]]; then
-    echo "Error: This isn't Travis!"
+    echo "Error: This isn't Travis" 2>&1
+    exit -1
+fi
+
+if (( $# < 1 )); then
+    echo "Error: Missing main tag" 2>&1
+    exit -1
 fi
 
 set -e # Exit on error
 set -u # Undefined var is error
 
 IMAGE=$DOCKER_REPO
-MAIN_TAG=$TRAVIS_COMMIT
+MAIN_TAG="$1"
+EXTRA_TAGS="${@:2}"
 
 echo "Logging into Docker Hub"
 echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
 
-echo "Building and deploying with unique tag"
+echo "Building and deploying with tag"
 docker build -t "$IMAGE:$MAIN_TAG" .
 docker push "$IMAGE:$MAIN_TAG"
 
-for extra_tag in "$@"; do
-    echo "Deploying with extra tag $extra_tag"
+for extra_tag in "$EXTRA_TAGS"; do
+    echo "Deploying with tag $extra_tag"
     docker tag "$IMAGE:$MAIN_TAG" "$IMAGE:$extra_tag"
     docker push "$IMAGE:$extra_tag"
 done
