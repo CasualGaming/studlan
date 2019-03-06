81c4eea652a8aa8ad51cba767f1733ca1821c829
Håvard O. Nordstrand <haavard.nordstrand@gmail.com>
2019-03-06 20:26:05 +0100

    Replace non-crypto lottery RNG with crypto RNG

--- apps/lottery/tests.py
+++ apps/lottery/tests.py
@@ -1,3 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from random import SystemRandom
+
 from django.test import TestCase
 
-# Create your tests here.
+
+class TeamTests(TestCase):
+
+    def test_systemrandom_support(self):
+        rand = SystemRandom()
+        num = rand.randint(510, 515)
+        self.assertTrue(num >= 510 and num <= 515)
