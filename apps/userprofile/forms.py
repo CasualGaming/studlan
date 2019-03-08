a44d342f3702ee2b0894e939eeb73abbc75127ab
Håvard O. Nordstrand <haavard.nordstrand@gmail.com>
2019-03-08 02:31:20 +0100

    Fix attribute error in user profile form

--- apps/userprofile/forms.py
+++ apps/userprofile/forms.py
@@ -33,12 +33,11 @@ class UserProfileForm(forms.ModelForm):
             # currently only checks that it is not after today
             date = cleaned_data['date_of_birth']
             if date >= datetime.date.today():
-                self.add_error('date_of_birth',
-                               _(u'You seem to be from the future, please enter a more believable date of birth.'))
+                self.add_error('date_of_birth', _(u'You seem to be from the future, please enter a more believable date of birth.'))
 
             # ZIP code digits only
             zip_code = cleaned_data['zip_code']
             if len(zip_code) != 4 or not zip_code.isdigit():
-                self._add_error('zip_code', _(u'The ZIP code must be 4 digit number.'))
+                self.add_error('zip_code', _(u'The ZIP code must be 4 digit number.'))
 
             return cleaned_data
