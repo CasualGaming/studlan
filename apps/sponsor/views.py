5c3671694ca762cd8d62749c4ff2dce9d0010800
Håvard O. Nordstrand <haavard@haavard.tech>
2019-07-21 05:57:27 +0200

    Make sponsor page prettier

--- apps/sponsor/views.py
+++ apps/sponsor/views.py
@@ -14,15 +14,9 @@ def index(request):
         sponsor_relations = SponsorRelation.objects.filter(lan__in=lans)
     else:
         sponsor_relations = []
-
     active_sponsors = [sponsor_relation.sponsor for sponsor_relation in sponsor_relations]
 
     sponsors = list(Sponsor.objects.all())
-
-    for sponsor in sponsors:
-        if not sponsor.banner:
-            sponsor.banner = 'http://placehold.it/300x150'
-
     inactive_sponsors = [sponsor for sponsor in sponsors if sponsor not in active_sponsors]
 
     return render(request, 'sponsor/sponsors.html', {'active_sponsors': active_sponsors, 'inactive_sponsors': inactive_sponsors})
