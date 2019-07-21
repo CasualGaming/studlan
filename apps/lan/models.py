f135fec718b9c19a363210cdf934e85e160b86bf
Håvard O. Nordstrand <haavard@haavard.tech>
2019-07-21 07:36:43 +0200

    Add video to LAN page

--- apps/lan/models.py
+++ apps/lan/models.py
@@ -14,7 +14,8 @@ class LAN(TranslatableModel):
     start_date = models.DateTimeField('start date')
     end_date = models.DateTimeField('end date')
     location = models.CharField('location', max_length=100)
-    map_link = models.CharField('map link', max_length=300, help_text='url for google maps embedded map', null=True)
+    map_link = models.CharField('map link', max_length=300, help_text='URL for an embedded map', blank=True)
+    video_link = models.CharField('video link', max_length=300, help_text='URL for an embedded video', blank=True)
 
     @property
     def attendees(self):
