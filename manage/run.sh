79f273876491f50e40728810f4fc94cf9167c0ad
Håvard O. Nordstrand <haavard@haavard.tech>
2019-08-13 15:12:05 +0200

    Add optional endpoint param to run script

--- manage/run.sh
+++ manage/run.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+# Run the dev server with hot reload. Uses either specified endpoint or localhost:8000 by default.
+
 LOCAL_DIR=".local/venv"
 MANAGE="manage/manage.sh"
-ENDPOINT="localhost:8000"
+DEFAULT_ENDPOINT="localhost:8000"
 
 set -eu
 
+if (( $# > 0 )); then
+    ENDPOINT="$1"
+else
+    ENDPOINT="$DEFAULT_ENDPOINT"
+fi
+
 echo "Starting Django dev server ..."
-$MANAGE runserver $ENDPOINT
+$MANAGE runserver "$ENDPOINT"
