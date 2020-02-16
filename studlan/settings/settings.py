8cc8d07cde3a278afb45e246ac21ccb09be56625
Håvard O. Nordstrand <haavard@haavard.tech>
2020-02-16 08:38:45 +0100

    Reorganize settings a bit

--- studlan/settings/settings.py
+++ studlan/settings/settings.py
@@ -74,9 +74,11 @@ TEMPLATES = [
 ]
 
 TEST_RUNNER = 'django.test.runner.DiscoverRunner'
-
+ROOT_URLCONF = 'studlan.urls'
+WSGI_APPLICATION = 'studlan.wsgi.application'
 AUTH_PROFILE_MODULE = 'userprofile.UserProfile'
 LOGIN_URL = '/auth/login/'
+MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
 
 # Locale information
 TIME_ZONE = 'Europe/Oslo'
@@ -85,53 +87,40 @@ SITE_ID = 1
 USE_I18N = True
 USE_L10N = True
 
+LOCALE_PATHS = (
+    os.path.join(PROJECT_ROOT_DIRECTORY, 'locale'),
+)
+
 LANGUAGES = (
     ('nb', u'Norsk'),
     ('en', u'English'),
 )
 
+# Media and static files
 # Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: '/home/media/media.lawrence.com/media/'
 MEDIA_ROOT = ''
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: 'http://media.lawrence.com/media/', 'http://example.com/media/'
+# URL that handles the media served from MEDIA_ROOT.
+# Make sure to use a trailing slash.
 MEDIA_URL = ''
-
 # Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' 'static/' subdirectories and in STATICFILES_DIRS.
-# Example: '/home/media/media.lawrence.com/static/'
 STATIC_ROOT = 'static/'
-
 # URL prefix for static files.
-# Example: 'http://media.lawrence.com/static/'
 STATIC_URL = '/static/'
-
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
-# Examples: 'http://foo.com/static/admin/', '/static/admin/'.
 ADMIN_MEDIA_PREFIX = '/static/admin/'
-
 # Additional locations of static files
 STATICFILES_DIRS = [
     os.path.join(PROJECT_ROOT_DIRECTORY, 'files'),
-    # Put strings here, like '/home/html/static' or 'C:/www/django/static'.
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
 ]
-
-# List of finder classes that know how to find static files in
-# various locations.
+# List of finder classes that know how to find static files in various locations.
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
     # 'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
-MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
-
+# Security
 CSRF_COOKIE_PATH = '/'
 CSRF_COOKIE_HTTPONLY = True
 CSRF_COOKIE_SECURE = True
@@ -139,14 +128,6 @@ SESSION_COOKIE_SECURE = True
 X_FRAME_OPTIONS = 'DENY'
 SECURE_CONTENT_TYPE_NOSNIFF = True
 SECURE_BROWSER_XSS_FILTER = True
-
-ROOT_URLCONF = 'studlan.urls'
-
-WSGI_APPLICATION = 'studlan.wsgi.application'
-
-LOCALE_PATHS = (
-    os.path.join(PROJECT_ROOT_DIRECTORY, 'locale'),
-)
 
 MARKDOWN_DEUX_STYLES = {
     'default': {
@@ -158,6 +139,7 @@ MARKDOWN_DEUX_STYLES = {
 }
 
 # Postman
+# Used for internal invitation messages
 POSTMAN_DISALLOW_ANONYMOUS = True
 POSTMAN_DISALLOW_MULTIRECIPIENTS = True
 POSTMAN_DISALLOW_COPIES_ON_REPLY = True
@@ -166,7 +148,7 @@ POSTMAN_AUTO_MODERATE_AS = True
 POSTMAN_SHOW_USER_AS = None
 POSTMAN_QUICKREPLY_QUOTE_BODY = False
 POSTMAN_NOTIFIER_APP = None
-POSTMAN_MAILER_APP = 'mailer'
+POSTMAN_MAILER_APP = None
 
 LOGGING = {
     'version': 1,
@@ -201,6 +183,7 @@ MAX_TEAMS = 10
 # Overiding messagetags to match bootstrap 3
 MESSAGE_TAGS = {message_constants.ERROR: 'danger'}
 
+# Version
 VERSION = 'Unknown version'
 version_file_path = os.path.join(PROJECT_ROOT_DIRECTORY, 'VERSION')
 if os.path.isfile(version_file_path):
@@ -212,6 +195,7 @@ if os.path.isfile(version_file_path):
 
 SHOW_VERSION = False
 
+# Local settings
 # Remember to keep 'local' last, so it can override any setting.
 for settings_module in ['local']:  # local last
     if not os.path.exists(os.path.join(PROJECT_SETTINGS_DIRECTORY, settings_module + '.py')):
