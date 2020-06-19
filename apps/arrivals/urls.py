dafed17a4b8064a121e9ef8f05d269cc27b6e20b
Håvard O. Nordstrand <haavard@haavard.tech>
2020-06-19 09:32:19 +0200

    Add LAN statistics page

--- apps/arrivals/urls.py
+++ apps/arrivals/urls.py
@@ -2,10 +2,11 @@
 
 from django.conf.urls import url
 
-from .views import arrivals, home, toggle
+from .views import arrivals, arrivals_statistics, home, toggle
 
 urlpatterns = [
     url(r'^$', home, name='arrival_home'),
     url(r'^(?P<lan_id>\d+)/$', arrivals, name='arrivals'),
+    url(r'^(?P<lan_id>\d+)/statistics/$', arrivals_statistics, name='arrivals_statistics'),
     url(r'^(?P<lan_id>\d+)/toggle/$', toggle, name='toggle'),
 ]
--- apps/misc/templatetags/misc.py
+++ apps/misc/templatetags/misc.py
@@ -41,3 +41,15 @@ def get_user_ticket(tickets, user):
 @register.filter
 def get_dict_val(dictionary, key):
     return dictionary.get(key)
+
+
+@register.simple_tag
+def divide(dividend, divisor):
+    if divisor == 0:
+        return 0
+    return dividend / float(divisor)
+
+
+@register.simple_tag
+def subtract(minuend, subtrahend):
+    return minuend - subtrahend
