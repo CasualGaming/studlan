2a7746ee937c90d902db6823d140cce8a4799cd1
HON95 <hon@hon.one>
2022-08-01 13:09:24 +0200

    Deduplicate user profile validation

--- apps/userprofile/models.py
+++ apps/userprofile/models.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
 
-from datetime import date
+import datetime
+import re
 
 from django.contrib.auth.models import User
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ugettext, ugettext_lazy as _
 
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, verbose_name=_(u'user'), related_name='profile')
     nick = models.CharField(_(u'nick'), max_length=20, db_index=True, help_text=_(u'Your display name. Should be equal or similar to your username.'))
-    date_of_birth = models.DateField(_(u'date of birth'), default=date.today)
+    date_of_birth = models.DateField(_(u'date of birth'), default=datetime.date.today)
     address = models.CharField(_(u'street address'), max_length=100)
     zip_code = models.CharField(_(u'postal code'), max_length=4)
     phone = models.CharField(_(u'phone number'), max_length=20)
@@ -18,12 +19,6 @@ class UserProfile(models.Model):
 
     def __unicode__(self):
         return self.user.username
-
-    def get_month(self):
-        return u'{0:02d}'.format(self.date_of_birth.month)
-
-    def get_day(self):
-        return u'{0:02d}'.format(self.date_of_birth.day)
 
     def has_address(self):
         if self.address and self.zip_code:
@@ -38,6 +33,43 @@ class UserProfile(models.Model):
         permissions = (
             ('show_private_info', u'Can show private user info'),
         )
+
+    @staticmethod
+    def check_username(username):
+        if User.objects.filter(username=username).count() > 0:
+            return ugettext(u'There is already a user with that username.')
+        if not re.match('^[a-zA-Z0-9_-]+$', username):
+            return ugettext(u'Your desired username contains illegal characters. Valid: a-Z 0-9 - _')
+        return None
+
+    @staticmethod
+    def check_nick(nick):
+        if not re.match('^[a-zA-Z0-9_-]+$', nick):
+            return ugettext(u'Your desired nickname contains illegal characters. Valid: a-Z 0-9 - _')
+        return None
+
+    @staticmethod
+    def check_date_of_birth(date_of_birth):
+        now = datetime.date.today()
+        if date_of_birth == now:
+            return ugettext(u'You seem to have been born today, that doesn\'t seem right.')
+        elif date_of_birth >= now:
+            return ugettext(u'You seem to be from the future, that doesn\'t seem right.')
+        elif date_of_birth < now.replace(year=(now.year - 150)):
+            return ugettext(u'You seem to be over 150 years old, that doesn\'t seem right.')
+        return None
+
+    @staticmethod
+    def check_zip_code(zip_code):
+        if len(zip_code) != 4 or not zip_code.isdigit():
+            return ugettext(u'The postal code must be a 4 digit number.')
+        return None
+
+    @staticmethod
+    def check_phone(phone):
+        if not re.match('^((\\+|00)[0-9]{2})?[0-9]{8,10}$', phone):
+            return ugettext(u'The phone number must consist of an optional country code followed by 8–10 digits (no spaces or symbols, but "+" allowed in country code).')
+        return None
 
 
 User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
